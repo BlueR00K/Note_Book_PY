8b45e45e9915c49789485f639ad7744d6825c85f
yashar M <yashar.man82@gmail.com>
2025-08-07 08:51:21 +0330

    refactor: Move all ASCII art and related code to ascii_art.py for cleaner main file

--- Function/functions_demo/adventure_game.py
+++ Function/functions_demo/adventure_game.py
@@ -4,6 +4,7 @@ Demo Project: Recursive Adventure Game
 This project demonstrates Python functions, generators, coroutines, decorators, function attributes, recursive generators, and memoization—all in a fun, interactive text adventure game.
 """
 
+from ascii_art import ASCII_GUIDES, DEFAULT_SWORD_ART
 from functools import lru_cache
 import time
 import sys
@@ -110,69 +111,6 @@ CAVE_LOC = (5, 2)
 
 def create_game_map():
     return generate_procedural_map()[0]
-
-
-# High-res (80x80) ASCII art for items and default Sword
-ASCII_GUIDES = {
-    '#': (r"""
-""" + "#"*80 + "\n"*79, "Wall: Blocks your path. Find a way around!"),
-    'T': (r"""
-""" + "$"*80 + "\n"*79, "Treasure: Collect for rewards!"),
-    'V': (r"""
-""" + "V"*80 + "\n"*79, "Vendor: Trade treasures for potions."),
-    'C': (r"""
-""" + "C"*80 + "\n"*79, "Cave: Face a challenge for a reward!"),
-    'M': (r"""
-""" + "M"*80 + "\n"*79, "Monster: Fight or flee!"),
-    'P': (r"""
-""" + "P"*80 + "\n"*79, "Potion: Restores health when used."),
-    'N': (r"""
-""" + "N"*80 + "\n"*79, "NPC: Meet a mysterious character. Maybe they have a quest!")
-}
-DEFAULT_SWORD_ART = (r"""
-                                                                      █         
-                                                                   █████        
-                                                                ████████        
-                                                              ██████████        
-                                                            ██████ ████         
-                                                          ██████ █████          
-                                                         ██████ ████            
-                                                       ████████████             
-                                                     ████ ████ ███              
-                                                   ███████ ██████               
-                                                  █████████ ███                 
-                                                ████ ██ ██████                  
-                                              ███████ ██ ████                   
-                                             ████ █████████                     
-                                           ████ ██ ██ ████                      
-                                         ███████████████                        
-                                        ████ ██ ██ ████                         
-                                       █████████ ████                           
-                                     ████ ██████████                            
-                                   ██████████ ████                              
-                                  ████ █████████                                
-                                ████ ██ ██ ████                                 
-                               ██████████████                                    
-                             ████ ██ ███████                                    
-                            █████████ ████                                      
-                   ███    ████ █████████                                        
-                  ████  ███████ ██ ████                                         
-                   ██████████████████                                           
-                   ██ ██████ ██ ████                                            
-                    █████ ████████                                              
-                   ████ █████████                                               
-                   ██ ██████████████                                            
-                  ██ █ ██████     ██                                            
-               ███ ██   ████████████                                            
-               █ █ ██ ██                                                        
-             ██ █ █████                                                         
-           ██████  ███                                                          
-        ████████████                                                            
-        ███ █████                                                               
-        ████████                                                                
-        ████████                                                                
-          █████                                                                 
-""" + "\n"*39, "The Sword: Your legendary weapon.")
 
 
 def draw_map(grid, player):
